9b233ea2fa5ee7951ba2f10c9fa54b7ade3f53a2
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-07-27 23:58:37 +0000

    Valid characters, length of 81

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,8 +2,14 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver();
 
 suite('Unit Tests', () => {
+
+  test('Valid characters, length of 81', (done) => {
+    const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+    assert.isTrue(solver.solve(puzzle).length === 81);
+    done();
+  });
 
 });
