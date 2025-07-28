4353a526087a89801a18e4db65fad06fe6bb36b4
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-07-28 02:53:49 +0000

    POST /api/check Check a puzzle placement with multiple placement conflicts

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -112,4 +112,23 @@ suite('Functional Tests', () => {
       });
   });
 
+  test('POST /api/check Check a puzzle placement with multiple placement conflicts', function(done) {
+    chai.request(server)
+      .post( '/api/check' )
+      .send( {
+        "puzzle": "1.576.984946381257728459613694517832812936745357824196473298561581673429269145378",
+        "coordinate": "A2",
+        "value": "5"
+      } )
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'valid');
+        assert.isFalse(res.body.valid);
+        assert.property(res.body, 'conflict');
+        assert.include(res.body.conflict, 'region');
+        assert.include(res.body.conflict, 'row');
+        done();
+      });
+  });
+
 });
