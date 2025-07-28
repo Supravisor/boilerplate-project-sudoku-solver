3a00365559c81e43922f1661754d7b8c933aaa18
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-07-28 00:04:22 +0000

    Valid row placement

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -26,4 +26,10 @@ suite('Unit Tests', () => {
     done();
   });
 
+  test('Valid row placement', function(done) {
+    const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+    assert.isTrue(solver.checkRowPlacement(puzzle, 0, 0, '7'));
+    done();
+  });
+
 });
