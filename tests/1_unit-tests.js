caabd2b6a2c7891a4cb2b1e2b23b902b4e4b555d
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-07-28 01:04:45 +0000

    Invalid column placement

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -44,4 +44,10 @@ suite('Unit Tests', () => {
     done();
   });
 
+  test('Invalid column placement', function(done) {
+    const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+    assert.isFalse(solver.checkColPlacement(puzzle, 0, 0, '8'));
+    done();
+  });
+
 });
