b9a9c0061442786d0144028c11f1d996eb9b9115
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-07-28 02:31:22 +0000

    POST /api/solve Solve a puzzle with valid puzzle string

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,5 +7,19 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
 
+  test('POST /api/solve Solve a puzzle with valid puzzle string', function(done) {
+    chai.request(server)
+      .post( '/api/solve' )
+      .send( {
+        "puzzle": "135762984946381257728459613694517832812936745357824196473298561581673429269145378"
+      } )
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'solution');
+        assert.isTrue(typeof Number(res.body.solution) === 'number');
+        done();
+      });
+  });
+
 });
 
