e30ab004b9fbbf531623db1d20b59a7a108c99b9
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-07-28 01:09:22 +0000

    Valid puzzle

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -62,4 +62,10 @@ suite('Unit Tests', () => {
     done();
   });
 
+  test('Valid puzzle', (done) => {
+    const puzzle = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+    assert.isTrue(/^\d+/.test(Number(solver.solve(puzzle))));
+    done();
+  });
+
 });
