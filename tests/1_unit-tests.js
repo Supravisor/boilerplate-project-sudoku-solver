0b7c0a059ef467109530e8ba7f9bcc99afaec7c1
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-07-28 00:02:44 +0000

    Invalid length

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -19,4 +19,11 @@ suite('Unit Tests', () => {
     done();
   });
 
+  test('Invalid length', (done) => {
+    const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.';
+    assert.property(solver.validate(puzzle), 'error');
+    assert.isString(solver.validate(puzzle).error, 'Expected puzzle length to be 81 characters long');
+    done();
+  });
+
 });
