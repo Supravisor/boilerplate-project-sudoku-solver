9d63f26ba287a4d6abe0714ccb9499d8c923fe23
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-07-28 01:06:15 +0000

    Invalid region placement

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -56,4 +56,10 @@ suite('Unit Tests', () => {
     done();
   });
 
+  test('Invalid region placement', function(done) {
+    const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+    assert.isFalse(solver.checkColPlacement(puzzle, 0, 1, '2'));
+    done();
+  });
+
 });
