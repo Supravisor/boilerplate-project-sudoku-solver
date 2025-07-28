d5dc856f58c21af5960626fb16dee386c578381b
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-07-28 02:49:59 +0000

    POST /api/solve Solve a puzzle with incorrect length

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -50,4 +50,18 @@ suite('Functional Tests', () => {
       });
   });
 
+  test('POST /api/solve Solve a puzzle with incorrect length', function(done) {
+    chai.request(server)
+      .post( '/api/solve' )
+      .send( {
+        "puzzle": "15..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37."
+      } )
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'error');
+        assert.include(res.body.error, 'Expected puzzle to be 81 characters long');
+        done();
+      });
+  });
+
 });
