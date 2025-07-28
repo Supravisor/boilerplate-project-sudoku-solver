cfee7e177fe163ec46dba22073e810f380e7657e
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-07-28 01:02:46 +0000

    Valid column placement

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -38,4 +38,10 @@ suite('Unit Tests', () => {
     done();
   });
 
+  test('Valid column placement', function(done) {
+    const puzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+    assert.isTrue(solver.checkColPlacement(puzzle, 0, 0, '3'));
+    done();
+  });
+
 });
